053d1564fc2ddbe6d6c1e72a516d57f458f75960
Chris-gqz <1395523356@qq.com>
2017-02-12 10:50:01 +0800

    rewrite the code

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// Declare app level module which depends on views, and components
+
 angular.module('moviecat', [
   'ngRoute',
     'moviecat.movie_detail',
@@ -20,31 +20,9 @@ angular.module('moviecat', [
     '$route',
      'AppConfig',
     function($scope,$route,AppConfig){
-
-
-
     $scope.input='';
     $scope.search=function(){
     $route.updateParams({category: 'search', q: $scope.input })
     }
 }])
-// .controller('NavController',[
-//   '$scope',
-//   '$location',
-//   function($scope,$location){
-//   $scope.$location = $location;
-//   $scope.$watch('$location.path()',function(now){
 
-//   if (now.startsWith('/in_theaters')) {
-//       $scope.type = 'in_theaters'
-//   } else  if (now.startsWith('/coming_soon')) {
-//       $scope.type = 'coming_soon'
-//   }else   if (now.startsWith('/top250')) {
-//       $scope.type = 'top250'
-//   };
-//     console.log($scope.type )
-//   })
-
-
-// }
-// ]);
